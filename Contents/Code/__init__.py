d85e76b6227d4e315d3a3cf4d46c1315c33cb014
solariz <cyberwhales@gmail.com>
2015-02-15 19:43:55 +0300

    загрузили файлы в репозиторий

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,50 @@
+ART = 'art-default.jpg'
+ICON = 'icon-default.jpg'
+NAME = 'Lepro fm'
+STREAM_URL = 'http://leproradio.com/vorbis.ogg'
+
+####################################################################################################
+def Start():
+
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	TrackObject.thumb = R(ICON)
+
+####################################################################################################     
+@handler('/music/resonancefm', NAME, thumb=ICON, art=ART)
+def MainMenu():
+
+	oc = ObjectContainer()
+	oc.add(CreateTrackObject(url=STREAM_URL, title=NAME))
+
+	return oc
+
+####################################################################################################
+def CreateTrackObject(url, title, include_container=False):
+
+	track_object = TrackObject(
+		key = Callback(CreateTrackObject, url=url, title=title, include_container=True),
+		rating_key = url,
+		title = title,
+		items = [
+			MediaObject(
+				parts = [
+					PartObject(key=Callback(PlayAudio, url=url, ext='ogg'))
+				],
+				container = Container.OGG,
+				bitrate = 128,
+				audio_codec = AudioCodec.VORBIS,
+				audio_channels = 2
+			)
+		]
+	)
+
+	if include_container:
+		return ObjectContainer(objects=[track_object])
+	else:
+		return track_object
+
+####################################################################################################
+def PlayAudio(url):
+
+	return Redirect(url)
